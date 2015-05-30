5bc83349d6a92d4a67096c3efc00418fadc2cf57
Bartosz Golek <bartosz.golek@gmail.com>
2015-05-30 15:45:05 +0200

    tests

---
+++ tests/Saigon/Conpago/Migrations/MigrateCommandPresenterTest.php
@@ -0,0 +1,41 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: bg
+	 * Date: 19.05.15
+	 * Time: 23:23
+	 */
+
+	namespace Saigon\Conpago\Migrations;
+
+
+	class MigrateCommandPresenterTest extends \PHPUnit_Framework_TestCase {
+
+		public function testMigrationStarted() {
+			$count = 1;
+
+			$consolePresenter = $this->getMock('Saigon\Conpago\Console\Contract\Presentation\IConsolePresenter');
+			$consolePresenter->expects($this->once())->method('write')->with($this->equalTo("Running migrations (".$count.")..."));
+
+			$migrateCommandPresenter = new MigrateCommandPresenter($consolePresenter);
+			$migrateCommandPresenter->migrationStarted($count);
+		}
+
+		public function testMigrationEnded() {
+			$consolePresenter = $this->getMock('Saigon\Conpago\Console\Contract\Presentation\IConsolePresenter');
+			$consolePresenter->expects($this->any())->method('write')->with($this->equalTo("Running migrations done."));
+
+			$migrateCommandPresenter = new MigrateCommandPresenter($consolePresenter);
+			$migrateCommandPresenter->migrationEnded();
+		}
+
+		public function testRunningMigration() {
+			$number = 1;
+			$count = 1;
+			$consolePresenter = $this->getMock('Saigon\Conpago\Console\Contract\Presentation\IConsolePresenter');
+			$consolePresenter->expects($this->any())->method('write')->with($this->equalTo("Running migration ".$number." of ". $count ."."));
+
+			$migrateCommandPresenter = new MigrateCommandPresenter($consolePresenter);
+			$migrateCommandPresenter->runningMigration($number, $count);
+		}
+	}
---
+++ tests/Saigon/Conpago/Migrations/MigrateCommandTest.php
@@ -0,0 +1,86 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: Bartosz Gołek
+	 * Date: 22.01.14
+	 * Time: 07:56
+	 */
+
+	namespace Saigon\Conpago\Migrations;
+
+	class MigrateCommandTest extends \PHPUnit_Framework_TestCase {
+
+		function testMigrateCommandReportsStart(){
+			$migrateCommandPresenter = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigrateCommandPresenter');
+			$migrateCommandPresenter->expects($this->once())->method('migrationStarted')->with($this->equalTo(0));
+
+			$migrateCommand = new MigrateCommand([], $migrateCommandPresenter);
+			$migrateCommand->execute();
+		}
+
+		function testMigrateCommandReportsStartWithMigrationsCount(){
+			$migrateCommandPresenter = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigrateCommandPresenter');
+			$migrateCommandPresenter->expects($this->once())->method('migrationStarted')->with($this->equalTo(1));
+
+
+			$migration = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigration');
+
+			$migrateCommand = new MigrateCommand([$migration], $migrateCommandPresenter);
+			$migrateCommand->execute();
+		}
+
+		function testMigrateCommandReportsEnd(){
+			$migrateCommandPresenter = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigrateCommandPresenter');
+			$migrateCommandPresenter->expects($this->once())->method('migrationEnded');
+
+			$migrateCommand = new MigrateCommand([], $migrateCommandPresenter);
+			$migrateCommand->execute();
+		}
+
+		function testMigrateCommandReportMigrationRun(){
+			$migration = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigration');
+
+			$migrateCommandPresenter = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigrateCommandPresenter');
+			$migrateCommandPresenter->expects($this->once())->method('runningMigration')->with($this->equalTo(1), $this->equalTo(1));
+
+			$migrateCommand = new MigrateCommand([$migration], $migrateCommandPresenter);
+			$migrateCommand->execute();
+		}
+
+		function testMigrateCommandReportAllMigrationsRun(){
+			$migration = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigration');
+
+			$migrateCommandPresenter = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigrateCommandPresenter');
+			$migrateCommandPresenter->expects($this->exactly(2))->method('runningMigration')
+				->withConsecutive(
+					array($this->equalTo(1), $this->equalTo(2)),
+					array($this->equalTo(2), $this->equalTo(2))
+				);
+
+			$migrateCommand = new MigrateCommand([$migration, $migration], $migrateCommandPresenter);
+			$migrateCommand->execute();
+		}
+
+		function testMigrateCommandReportRunsMigration(){
+			$migration = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigration');
+			$migration->expects($this->once())->method('up');
+
+			$migrateCommandPresenter = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigrateCommandPresenter');
+
+			$migrateCommand = new MigrateCommand([$migration], $migrateCommandPresenter);
+			$migrateCommand->execute();
+		}
+
+		function testMigrateCommandRunsAllMigrations(){
+			$migration = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigration');
+			$migration->expects($this->once())->method('up');
+
+			$migration2 = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigration');
+			$migration2->expects($this->once())->method('up');
+
+			$migrateCommandPresenter = $this->getMock('Saigon\Conpago\Migrations\Contract\IMigrateCommandPresenter');
+
+			$migrateCommand = new MigrateCommand([$migration, $migration2], $migrateCommandPresenter);
+			$migrateCommand->execute();
+		}
+	}
